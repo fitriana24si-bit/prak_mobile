e70a43ac964cde343d18944bce5935ff1a9a93a0
Fitriana Tasya <fitriana24si@mahasiswa.pcr.ac.id>
2026-03-11 09:35:20 +0700

    first commit

---
+++ app/src/main/java/com/example/anacoffe/pertemuan_2/BelajarKotlin.kt
@@ -0,0 +1,45 @@
+package com.example.anacoffe.pertemuan_2
+
+fun main(){
+    println("Hai rekan-rekan...")
+    println("Selamat Datang di bahasa pemrograman kotlin")
+
+    println("=============")
+
+    var angka = 15
+    println("Hasil dari 15 + 10 = ${angka + 10}")
+
+    val nilaiInt = 10000
+    val nilaiDouble = 100.003
+    val nilaiFloat= 1000.0f
+
+    println("Nilai Integer = $nilaiInt")
+    println("Nilai Double = $nilaiDouble")
+    println("Nilai Float = $nilaiFloat")
+
+    println("========STRING========")
+    val huruf = 'a'
+    println("ini penggunaaan karakter '$huruf'")
+
+    val nilaiString = "Mawar"
+    println("Halo $nilaiString!\nApa kabar?")
+
+    println("=======KONDISI=======")
+
+    val nilai = 10
+    if(nilai<0)
+        println("bilangan negatif")
+    else {
+        if(nilai%2 == 0)
+            println("bilangan genap")
+        else
+            println("bilangan ganjil")
+
+
+    }
+    println("======perulangan====")
+    val kampusKu: Array<String> = arrayOf("Kampus", "Politeknik","Caltex", "Riau")
+    for (kampus: String in kampusKu){
+        println(kampus)
+    }
+}

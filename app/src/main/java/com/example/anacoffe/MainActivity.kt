580223daddff86cd8150a3c31dcd93d57019773c
fitriana24si-bit <fitriana24si@mahasiswa.pcr.ac.id>
2026-04-16 00:00:40 +0700

    Pertemuan5

--- app/src/main/java/com/example/anacoffe/MainActivity.kt
+++ app/src/main/java/com/example/anacoffe/MainActivity.kt
@@ -1,44 +1,48 @@
 package com.example.anacoffe
 
+
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.anacoffe.databinding.ActivityMainBinding
-import com.example.anacoffe.databinding.ActivityThirdBinding
-import com.example.anacoffe.pertemuan_3.ThirdResultActivity
 import com.example.anacoffe.pertemuan_4.FourthActivity
+import com.example.anacoffe.pertemuan_5.FifthActivity
+import com.example.anacoffe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
 
+        // 🔥 INIT BINDING
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        // 🔥 HANDLE SAFE AREA
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-            binding.btntofourth.setOnClickListener {
-                Toast.makeText(
-                    this,
-                    "Pesan berhasil terkrim ke ",
-                    Toast.LENGTH_SHORT).show()
 
-                val intent = Intent(this, FourthActivity::class.java)
-                intent.putExtra("nama", "Politeknik Caltex Riau")
-                intent.putExtra("asal", "Rumbai")
-                intent.putExtra("usia", 25)
-
-                startActivity(intent)
-                finish()
-            }
+        // 🔵 KE PERTEMUAN 4
+        binding.btnToFourth.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("nama", "Politeknik Caltex Riau")
+            intent.putExtra("asal", "Rumbai")
+            intent.putExtra("usia", 25)
+            startActivity(intent)
+        }
+        // 🔵 KE PERTEMUAN 5
+        binding.btnToFifth.setOnClickListener {
+            val intent = Intent(this, FifthActivity::class.java)
+            startActivity(intent)
         }
     }
+}
